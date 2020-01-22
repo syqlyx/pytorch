jasperzhong/swift

Add assert_tensor_equal and assert_tensor_not_equal to test/cpp/api/support.h
gh-metadata: pytorch pytorch 30426 gh/yf225/88/head

test/cpp/api/support.h
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 
+#include <ATen/native/TensorIndexing.h>
 #include <torch/nn/cloneable.h>
 #include <torch/types.h>
 #include <torch/utils.h>
@@ -47,6 +48,83 @@ private:
 
 inline bool pointer_equal(at::Tensor first, at::Tensor second) {
   return first.data_ptr<float>() == second.data_ptr<float>();
+}
+
+// This mirrors the `isinstance(x, torch.Tensor) and isinstance(y, Number)` branch
+// in `TestCase.assertEqual` in test/common_utils.py
+#define TENSOR(T, S) \
+inline void assert_tensor_equal(at::Tensor a, T b) { \
+  ASSERT_TRUE(std::abs(a.item<T>() - b) < 1e-4); \
+}
+AT_FORALL_SCALAR_TYPES_AND3(Bool, Half, BFloat16, TENSOR)
+#undef TENSOR
+
+// This mirrors the `isinstance(x, torch.Tensor) and isinstance(y, torch.Tensor)` branch
+// in `TestCase.assertEqual` in test/common_utils.py
+inline void assert_tensor_equal(at::Tensor a, at::Tensor b, bool allow_inf=false) {
+  ASSERT_TRUE(a.sizes() == b.sizes());
+  if (a.numel() > 0) {
+    if (a.device().type() == torch::kCPU && (a.dtype() == torch::kFloat16 || a.dtype() == torch::kBFloat16)) {
+      // CPU half and bfloat16 tensors don't have the methods we need below
+      a = a.to(torch::kFloat32);
+    }
+    b = b.to(a);
+
+    if ((a.dtype() == torch::kBool) != (b.dtype() == torch::kBool)) {
+      TORCH_CHECK(false, "Was expecting both tensors to be bool type.");
+    } else {
+      if (a.dtype() == torch::kBool && b.dtype() == torch::kBool) {
+        // we want to respect precision but as bool doesn't support substraction,
+        // boolean tensor has to be converted to int
+        a = a.to(torch::kInt);
+        b = b.to(torch::kInt);
+      }
+
+      auto diff = a - b;
+      if (a.is_floating_point()) {
+        // check that NaNs are in the same locations
+        auto nan_mask = torch::isnan(a);
+        ASSERT_TRUE(torch::equal(nan_mask, torch::isnan(b)));
+        diff.idx_put_({nan_mask}, 0);
+        // inf check if allow_inf=true
+        if (allow_inf) {
+          auto inf_mask = torch::isinf(a);
+          auto inf_sign = inf_mask.sign();
+          ASSERT_TRUE(torch::equal(inf_sign, torch::isinf(b).sign()));
+          diff.idx_put_({inf_mask}, 0);
+        }
+      }
+      // TODO: implement abs on CharTensor (int8)
+      if (diff.is_signed() && diff.scalar_type() != torch::kInt8) {
+        diff = diff.abs();
+      }
+      auto max_err = diff.max().item<double>();
+      ASSERT_LE(max_err, 1e-5);
+    }
+  }
+}
+
+// This mirrors the `isinstance(x, torch.Tensor) and isinstance(y, torch.Tensor)` branch
+// in `TestCase.assertNotEqual` in test/common_utils.py
+inline void assert_tensor_not_equal(at::Tensor x, at::Tensor y) {
+  if (x.sizes() != y.sizes()) {
+    return;
+  }
+  ASSERT_GT(x.numel(), 0);
+  y = y.type_as(x);
+  y = x.is_cuda() ? y.to({torch::kCUDA, x.get_device()}) : y.cpu();
+  auto nan_mask = x != x;
+  if (torch::equal(nan_mask, y != y)) {
+    auto diff = x - y;
+    if (diff.is_signed()) {
+      diff = diff.abs();
+    }
+    diff.idx_put_({nan_mask}, 0);
+    // Use `item()` to work around:
+    // https://github.com/pytorch/pytorch/issues/22301
+    auto max_err = diff.max().item<double>();
+    ASSERT_GE(max_err, 1e-5);
+  }
 }
 
 inline int count_substr_occurrences(const std::string& str, const std::string& substr) {

syqlyx/pytorch

test/cpp/api/support.h
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 
+#include <ATen/native/TensorIndexing.h>
 #include <torch/nn/cloneable.h>
 #include <torch/types.h>
 #include <torch/utils.h>
@@ -47,6 +48,87 @@ private:
 
 inline bool pointer_equal(at::Tensor first, at::Tensor second) {
   return first.data_ptr() == second.data_ptr();
+}
+
+// This mirrors the `isinstance(x, torch.Tensor) and isinstance(y, Number)` branch
+// in `TestCase.assertEqual` in test/common_utils.py
+#define TENSOR(T, S) \
+inline void assert_tensor_equal(at::Tensor a, T b) { \
+  ASSERT_TRUE(std::abs(a.item<T>() - b) < 1e-4); \
+}
+AT_FORALL_SCALAR_TYPES_AND3(Bool, Half, BFloat16, TENSOR)
+#undef TENSOR
+
+// This mirrors the `isinstance(x, torch.Tensor) and isinstance(y, torch.Tensor)` branch
+// in `TestCase.assertEqual` in test/common_utils.py
+inline void assert_tensor_equal(at::Tensor a, at::Tensor b, bool allow_inf=false) {
+  ASSERT_TRUE(a.sizes() == b.sizes());
+  if (a.numel() > 0) {
+    if (a.device().type() == torch::kCPU && (a.dtype() == torch::kFloat16 || a.dtype() == torch::kBFloat16)) {
+      // CPU half and bfloat16 tensors don't have the methods we need below
+      a = a.to(torch::kFloat32);
+    }
+    if (a.device().type() == torch::kCUDA && a.dtype() == torch::kBFloat16) {
+      // CUDA bfloat16 tensors don't have the methods we need below
+      a = a.to(torch::kFloat32);
+    }
+    b = b.to(a);
+
+    if ((a.dtype() == torch::kBool) != (b.dtype() == torch::kBool)) {
+      TORCH_CHECK(false, "Was expecting both tensors to be bool type.");
+    } else {
+      if (a.dtype() == torch::kBool && b.dtype() == torch::kBool) {
+        // we want to respect precision but as bool doesn't support subtraction,
+        // boolean tensor has to be converted to int
+        a = a.to(torch::kInt);
+        b = b.to(torch::kInt);
+      }
+
+      auto diff = a - b;
+      if (a.is_floating_point()) {
+        // check that NaNs are in the same locations
+        auto nan_mask = torch::isnan(a);
+        ASSERT_TRUE(torch::equal(nan_mask, torch::isnan(b)));
+        diff.index_put_({nan_mask}, 0);
+        // inf check if allow_inf=true
+        if (allow_inf) {
+          auto inf_mask = torch::isinf(a);
+          auto inf_sign = inf_mask.sign();
+          ASSERT_TRUE(torch::equal(inf_sign, torch::isinf(b).sign()));
+          diff.index_put_({inf_mask}, 0);
+        }
+      }
+      // TODO: implement abs on CharTensor (int8)
+      if (diff.is_signed() && diff.scalar_type() != torch::kInt8) {
+        diff = diff.abs();
+      }
+      auto max_err = diff.max().item<double>();
+      ASSERT_LE(max_err, 1e-5);
+    }
+  }
+}
+
+// This mirrors the `isinstance(x, torch.Tensor) and isinstance(y, torch.Tensor)` branch
+// in `TestCase.assertNotEqual` in test/common_utils.py
+inline void assert_tensor_not_equal(at::Tensor x, at::Tensor y) {
+  if (x.sizes() != y.sizes()) {
+    return;
+  }
+  ASSERT_GT(x.numel(), 0);
+  y = y.type_as(x);
+  y = x.is_cuda() ? y.to({torch::kCUDA, x.get_device()}) : y.cpu();
+  auto nan_mask = x != x;
+  if (torch::equal(nan_mask, y != y)) {
+    auto diff = x - y;
+    if (diff.is_signed()) {
+      diff = diff.abs();
+    }
+    diff.index_put_({nan_mask}, 0);
+    // Use `item()` to work around:
+    // https://github.com/pytorch/pytorch/issues/22301
+    auto max_err = diff.max().item<double>();
+    ASSERT_GE(max_err, 1e-5);
+  }
 }
 
 inline int count_substr_occurrences(const std::string& str, const std::string& substr) {